e4d0205c12f0e39fb3bdb511285f5b9ccb3de9a7
Phichitphon Kaeosoda <phichit235@gmail.com>
2025-11-23 15:18:18 +0700

    feat: Implement functional mobile Hamburger Menu toggle.
    Introduced JavaScript logic (toggleHam function) in main.js to control menu state.
    - Updated HTML to include 'hamburger-icon' and set 'id="myMenu"'.
    - Added 'menu-active' styles and media query definitions in style.css for responsive display.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function toggleHam(x) {
   x.classList.toggle("change");
+
+  let myMenu = document.getElementById("myMenu");
+  if (myMenu.className === "nav-wrapper") {
+    myMenu.className += " menu-active";
+}else{
+    myMenu.className = "nav-wrapper";
+}
 }
